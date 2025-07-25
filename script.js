e4841b3b08cb63ee5735df95db80c4fb41b3890a
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2025-07-25 13:17:47 +0200

    done

--- script.js
+++ script.js
@@ -85,3 +85,107 @@ console.log( "productInfo" in virtualPet ) // true
 console.log( "volume" in virtualPet ) // false
 
 console.log( Object.keys(virtualPet) )
+
+
+const newVirtualPet = {
+  name: "Pugsy",
+  version: 1.0,
+  isHappy: true,
+  favFoods: [ "fish", "carrot", "apple", "banana" ],
+
+  greet() {
+    // console.log(this) // when we use the keyword this in a method, it points to the object that is calling the method
+    console.log(`Hello!, my name is ${this.name}`)
+  },
+
+  changeMood() {
+    if (this.isHappy === true ) {
+      this.isHappy = false
+      console.log(`Your virtual pet ${this.name} is now mad at you!`)
+    } else {
+      this.isHappy = true
+      console.log(`Your virtual pet ${this.name} is now happy and loves you!`)
+    }
+  },
+
+  eatRandomFood() {
+    // it will get a random element from the array and print: "pet is eating ____"
+
+    // Math.random() // 0 - 0.99999
+    let randomNum = Math.random()
+    let randomNum0to2 = randomNum * this.favFoods.length
+    let randomIndex = Math.floor(randomNum0to2)
+    let randomFood = this.favFoods[randomIndex]
+   
+    console.log(`Your pet ${this.name} is eating some ${randomFood}`)
+
+  }
+
+}
+
+
+newVirtualPet.greet()
+
+newVirtualPet.name = "pikachu"
+
+newVirtualPet.greet()
+
+newVirtualPet.changeMood()
+newVirtualPet.changeMood()
+
+newVirtualPet.eatRandomFood()
+
+
+//* Data Structure
+const users = [
+  {
+    username: "samuel",
+    salary: 20000,
+    occupation: "junior",
+  },
+  {
+    username: "John",
+    salary: 22000,
+    occupation: "junior II",
+  },
+  {
+    username: "Sara",
+    salary: 36000,
+    occupation: "senior",
+  },
+  {
+    username: "Luis",
+    salary: 42000,
+    occupation: "PM",
+  },
+];
+
+// print the second object
+console.log(users[1].occupation)
+
+// change the name of the last user from Luis to Bob
+users[users.length - 1].username = "bob"
+
+// want to increase the salary of the first user by 1000
+users[0].salary += 1000
+console.log(users)
+
+// new user "ruth", "GM", "52000"
+
+const newUser = {
+  username: "Ruth",
+  salary: 52000,
+  occupation: "GM",
+}
+
+users.push(newUser)
+
+// we want to increase the salaries of everyone by 10%
+for (let i = 0; i < users.length; i++) {
+  let user = users[i]
+  console.log(user)
+  // user.salary = user.salary * 1.1
+  user.salary *= 1.1
+}
+
+console.log(users)
